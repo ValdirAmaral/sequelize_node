636970d276daceb083146c8d47338e6f75505d16
ValdirAmaral <90727281+ValdirAmaral@users.noreply.github.com>
2022-10-20 13:17:38 -0300

    squelize

--- routes.js
+++ routes.js
@@ -1,9 +1,12 @@
-import express, { Router } from "express"
+import express from "express";
+import clients from "./src/controllers/clients.js";
 
-const routes = express.Router()
+const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    return res.json({name: "Ciclano Fulano"})
-})
+routes.get("/clients", clients.findAll);
+routes.post("/clients", clients.addClient);
+routes.get("/clients/:id", clients.findClient);
+routes.put("/clients/:id", clients.updateClient);
+routes.delete("/clients/:id", clients.deleteClient);
 
-export { routes as default }
+export { routes as default };
---
+++ src/models/clientsModels.js
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize";
+import db from "../db.js"
+
+export default db.define("Client", {
+
+    id: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+})
